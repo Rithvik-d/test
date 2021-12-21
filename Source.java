3d950e5202e38ecfaf6d2daba1f3bf0bd38c3d6d
Rithvik-d <95231603+Rithvik-d@users.noreply.github.com>
2021-12-21 18:49:33 +0530

    homework dec21

--- Source.java
+++ Source.java
@@ -1,103 +1,55 @@
 package com.company;
 import java.util.Scanner;
-public class Source {
+public class Main {
     public static void main(String[] args) {
-        int[] arr = {7, 7, 7, 7, 7, 7, 8, 8, 8, 9, 10, 12, 13, 15};
-        int[] descArr = {15, 13, 12, 10, 9, 7, 4, 2, 1};
-
-        int number = 7;
-        int index = getIndexFromAscendingBinarySearch(arr, number);
-        int c = getFirstIndexBinarySearchAscending(arr, number);
-        int b = getIndexFromDescendingBinarySearch(descArr, number);
-        System.out.println(index == -1 ? ("The number " + number + " is not present in the array.") :
-                ("Got the number " + number + " at index position " + index));
-        System.out.println(b == -1 ? ("The number for FromDescendingBinarySearch " + number + " is not present in the array.") :
-                ("Got the number for FromDescendingBinarySearch " + number + " at index position " + b));
-        System.out.println(c == -1 ? ("The number for FirstIndexBinarySearchAscending " + number + " is not present in the array.") :
-                ("Got the number for FirstIndexBinarySearchAscending " + number + " at index position " + c));
+        System.out.println("Set bit count : " + getSetBitCount(12)); // 15 = 1111
+        System.out.println("Set bit count : " + getdifferentBitCount(15,16));
+        getdecToBinary(12);
     }
 
-    public static int getIndexFromAscendingBinarySearch(int[] arr, int number) {
-        int index = -1;
-
-        int l = 0, r = arr.length - 1, mid;
-        while (l <= r) {
-            System.out.println("l = " + l + ", r = " + r);
-            mid = (l + r) / 2;
-
-            if (arr[mid] > number) {
-                r = mid - 1;
-            } else if (arr[mid] < number) {
-                l = mid + 1;
-            } else {
-                index = mid;
-                break;
+    public static int getSetBitCount(int n) {
+        int count = 0, pow = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n & pow) > 0) {
+                // bit at i th position is set
+                count++;
             }
+            pow <<= 1;
         }
-
-        return index;
+        return count;
     }
 
-    public static int getIndexFromDescendingBinarySearch(int[] arr, int number) {
-        int index = -1;
+// Get the number of different bits of 2 numbers.
+// Given A & B.how many bits are different among them
+// 10000 => 16
+// 01111 => 15
+// 0010 -> count set bits
 
-        int l = 0, r = arr.length - 1, mid;
-        while (l <= r) {
-            System.out.println("dl = " + l + ", dr = " + r);
-            mid = (l + r) / 2;
-
-            if (arr[mid] < number) {
-                r = mid - 1;
-            } else if (arr[mid] > number) {
-                l = mid + 1;
-            } else {
-                if (arr[mid] == number) {
-                    for (int i = l; i <= r; i++) {
-                        if (arr[i] == arr[mid]) {
-                            index = i;
-                            break;
-                        }
-                    }
-                }
-
-                break;
+    public static int getdifferentBitCount(int n1,int n2){
+        int count = 0, pow = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n1 & pow)!=(n2 & pow) ) {
+                // bit at i th position is set
+                count++;
             }
+            pow <<= 1;
         }
-
-        return index;
+        return count;
     }
+// decimal to binary
+public static void getdecToBinary(int n)
+{
 
-
-    public static int getFirstIndexBinarySearchAscending(int[] arr, int number) {
-        // If the number is repeated in the array, return the left most index,
-        // i.e. for the array
-        // {7, 7, 7, 7, 7, 7, 8, 8, 8, 9, 10, 12, 13, 15} -> return 0
-        int index = -1;
-        int l = 0, r = arr.length - 1, mid;
-        while (l <= r) {
-            System.out.println("l = " + l + ", r = " + r);
-            mid = (l + r) / 2;
-
-            if (arr[mid] > number) {
-                r = mid - 1;
-            } else if (arr[mid] < number) {
-                l = mid + 1;
-            } else {
-                if (arr[mid] == number) {
-                    for (int i = l; i <= r; i++) {
-                        if (arr[i] == arr[mid]) {
-                            index = i;
-                            break;
-                        }
-                    }
-                }
-
-                break;
-            }
-        }
-
-        return index;
-
-
+    for (int i = 4; i >= 0; i--) {
+        int k = n >> i;
+        if ((k & 1) > 0)
+            System.out.print("1");
+        else
+            System.out.print("0");
     }
+}
+
+
+
+
 }
