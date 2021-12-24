ab2304c0c61bc7235a3332c87d41eb2759b0c3a8
Rithvik-d <95231603+Rithvik-d@users.noreply.github.com>
2021-12-24 19:32:09 +0530

    HW dec 24

--- Source.java
+++ Source.java
@@ -1,55 +1,30 @@
 package com.company;
-import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Set bit count : " + getSetBitCount(12)); // 15 = 1111
-        System.out.println("Set bit count : " + getdifferentBitCount(15,16));
-        getdecToBinary(12);
+        // write your code here
+
+        permutationString("abcd");
     }
 
-    public static int getSetBitCount(int n) {
-        int count = 0, pow = 1;
-        for (int i = 0; i < 32; i++) {
-            if ((n & pow) > 0) {
-                // bit at i th position is set
-                count++;
-            }
-            pow <<= 1;
-        }
-        return count;
-    }
+    public static void permutationString(String a) {
 
-// Get the number of different bits of 2 numbers.
-// Given A & B.how many bits are different among them
-// 10000 => 16
-// 01111 => 15
-// 0010 -> count set bits
+        StringBuilder b = new StringBuilder(a);
+     char c,d;
 
-    public static int getdifferentBitCount(int n1,int n2){
-        int count = 0, pow = 1;
-        for (int i = 0; i < 32; i++) {
-            if ((n1 & pow)!=(n2 & pow) ) {
-                // bit at i th position is set
-                count++;
-            }
-            pow <<= 1;
-        }
-        return count;
-    }
-// decimal to binary
-public static void getdecToBinary(int n)
-{
 
-    for (int i = 4; i >= 0; i--) {
-        int k = n >> i;
-        if ((k & 1) > 0)
-            System.out.print("1");
-        else
-            System.out.print("0");
+for(int i=0;i<a.length();i++ ){
+    for(int j=0;j<a.length()-i-1;j++){
+        c=b.charAt(j);
+        d=b.charAt(j+1);
+        b.setCharAt(j, d);
+        b.setCharAt(j+1, c);
+
+        System.out.println(b);
     }
 }
 
 
-
-
+    }
 }
